1faa0a17a56f47242f13d2a392d6e40a42517a2d
wwhhooami <35835444+AutApple@users.noreply.github.com>
2026-03-13 19:07:25 +0300

    export everything in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { Rectangle } from './shapes/rectangle.shape.js';
-import { Triangle } from './shapes/triangle.shape.js';
-
-const rect = new Rectangle({ width: 10, height: 5 });
-
-const triangle = new Triangle({ a: 2, b: 5, c: 3 });
-
-
-console.log(rect.getArea());
-console.log(rect.getPerimeter());
-
-console.log(rect.getDimensions());
-
-console.log(rect.toString());
+export * from './shapes/index.js';
